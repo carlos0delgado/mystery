c203764e0429b3ac1ecab5e7f99f6071f14ac9da
carlos0delgado <carlos_96_delgado@hotmail.com>
2022-05-15 09:49:44 -0500

    initial

--- survey/__manifest__.py
+++ survey/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Surveys',
+    'name': 'mystery',
     'version': '5.0',
     'category': 'Marketing/Surveys',
     'description': """ Carlos Delgado
@@ -10,7 +10,7 @@ Create beautiful surveys and visualize answers
 
 It depends on the answers or reviews of some questions by different users. A
 survey may have multiple pages. Each page may contain multiple questions and
-each question may have multiple answers. Different users may give different
+each question may have multiple answers. Different usurveysers may give different
 answers of question and according to that survey is done. Partners are also
 sent mails with personal token for the invitation of the survey.
     """,
